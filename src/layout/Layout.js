6f766090610add0ea79f2ce1c024d3115b030338
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-02 22:06:09 -0300

    Creado campo y método para actualizar estado

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,11 +1,41 @@
 import React from 'react';
 
 class Layout extends React.Component {
-    render() {
-        return (
-            <p>Hola, mundo!</p>
-        );
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: 'Text',
+    };
+
+    this.changeText = this.changeText.bind(this);
+  }
+
+  changeText(event) {
+    var text = event.target.value;
+    this.setState({ data: text });
+  }
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-12 col-lg-4">
+            <div className="form-group">
+              <label>Edit text</label>
+              <input
+                type="text"
+                className="form-control"
+                value={this.state.data}
+                onChange={this.changeText}
+              />
+            </div>
+          </div>
+        </div>
+        <hr />
+        <p>Value of input: {this.state.data}</p>
+      </div>
+    );
+  }
 }
 
 export default Layout;
